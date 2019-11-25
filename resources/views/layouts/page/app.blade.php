1c10dbbf5591aaa7a24d7c222feddb0a8624987f
Hidalgo Mac <hidalgoponce@icloud.com>
2019-11-25 16:08:31 -0500

    Update

--- resources/views/layouts/page/app.blade.php
+++ resources/views/layouts/page/app.blade.php
@@ -143,8 +143,15 @@
             $('[data-toggle="tooltip"]').tooltip()
         })
     </script>
-<script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDf1RN8KKGNdS-iEarIgXpaqa-khw7EmZI&callback=initMap">
-</script>
+    <!-- begin olark code -->
+    <script type="text/javascript" async> ;(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");
+        /* custom configuration goes here (www.olark.com/documentation) */
+        olark.identify('7508-290-10-8562');</script>
+    <!-- end olark code -->
+
+
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDf1RN8KKGNdS-iEarIgXpaqa-khw7EmZI&callback=initMap">
+    </script>
 </body>
 </html>
