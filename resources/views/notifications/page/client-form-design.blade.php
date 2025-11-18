64a146491b9a7fd6f80b21ec96e87744a5eb53fe
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2025-11-18 11:00:58 -0500

    fix:

--- resources/views/notifications/page/client-form-design.blade.php
+++ resources/views/notifications/page/client-form-design.blade.php
@@ -84,36 +84,36 @@
                 <p>Un agente lo contactará con las primeras propuestas de viaje.</p>
               </td>
             </tr>
-            <tr>
-              <td style="padding:30px 10px 35px 10px;font-size:0;background-color:#ffffff;text-align:center;border-bottom:1px solid #f0f0f5;border-color:rgba(201,201,207,.35)">
-                <p style="margin:0;font-weight: 600;COLOR:#7F8429;text-align:Center;padding-bottom:20px;font-size:18px">¿Deseas conversar en este momento?</p>
-                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">
-                  <a href="https://gotoperu.com.mx/formularioZoom" target="_blank"><img src="{{asset('images/zoom.png')}}" alt="zoom" width="64" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>
-                </div>
-                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">
-                  <a href="https://meet.google.com/qau-xuon-bvr?pli=1&authuser=2" target="_blank" style="text-decoration:none;color:#464646;"><img src="{{asset('images/hangout.png')}}" alt="meet" width="64" style="margin:0 auto;display:block;border:none; padding:0 10px;"></a>
-                  <p style="font-size:11px;line-height:14px;color:#6c757d">Reunión Inmediata<br>Horario 09:00a.m 18:00p.m.</p>
-                </div>
-                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">
-                  <a href="https://api.whatsapp.com/send?phone=51922300244" target="_blank"><img src="{{asset('images/whatsapp.png')}}" alt="whatsaap" width="64" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>
-                </div>
-                <!--
-                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">
-                  <a href="https://www.facebook.com/GOTOPERUcom/" target="_blank"><img src="" alt="zoom" width="55" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>
-                </div>
-              -->
-                <a href="https://www.callmebackwidget.com/" target="_blank" style="display:block;font-weight:bolder;background-color:#FFC600;line-height:15px;padding:10px 10px;border-radius: 5px;font-size:16px;color:#ffffff;text-decoration:none;">Nosotros te llamamos <span style="font-size:13px">(agendar llamada)</span></a>
-              </td>
-            </tr>
+{{--            <tr>--}}
+{{--              <td style="padding:30px 10px 35px 10px;font-size:0;background-color:#ffffff;text-align:center;border-bottom:1px solid #f0f0f5;border-color:rgba(201,201,207,.35)">--}}
+{{--                <p style="margin:0;font-weight: 600;COLOR:#7F8429;text-align:Center;padding-bottom:20px;font-size:18px">¿Deseas conversar en este momento?</p>--}}
+{{--                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">--}}
+{{--                  <a href="https://gotoperu.com.mx/formularioZoom" target="_blank"><img src="{{asset('images/zoom.png')}}" alt="zoom" width="64" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>--}}
+{{--                </div>--}}
+{{--                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">--}}
+{{--                  <a href="https://meet.google.com/qau-xuon-bvr?pli=1&authuser=2" target="_blank" style="text-decoration:none;color:#464646;"><img src="{{asset('images/hangout.png')}}" alt="meet" width="64" style="margin:0 auto;display:block;border:none; padding:0 10px;"></a>--}}
+{{--                  <p style="font-size:11px;line-height:14px;color:#6c757d">Reunión Inmediata<br>Horario 09:00a.m 18:00p.m.</p>--}}
+{{--                </div>--}}
+{{--                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">--}}
+{{--                  <a href="https://api.whatsapp.com/send?phone=51922300244" target="_blank"><img src="{{asset('images/whatsapp.png')}}" alt="whatsaap" width="64" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>--}}
+{{--                </div>--}}
+{{--                <!----}}
+{{--                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">--}}
+{{--                  <a href="https://www.facebook.com/GOTOPERUcom/" target="_blank"><img src="" alt="zoom" width="55" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>--}}
+{{--                </div>--}}
+{{--              -->--}}
+{{--                <a href="https://www.callmebackwidget.com/" target="_blank" style="display:block;font-weight:bolder;background-color:#FFC600;line-height:15px;padding:10px 10px;border-radius: 5px;font-size:16px;color:#ffffff;text-decoration:none;">Nosotros te llamamos <span style="font-size:13px">(agendar llamada)</span></a>--}}
+{{--              </td>--}}
+{{--            </tr>--}}
             <tr>
               <td style="padding:30px;background-color:#FFFFFF;">
                 <p style="margin:0;font-weight: 600;COLOR:#7F8429;text-align:center;font-size:18px;margin-bottom:10px">¿Por qué GOTOPERU?</p>
                 <p style="margin:0;font-weight: 600;COLOR:#f9a825;padding:5px 10px;font-size:15px">Reserve con flexibilidad<br>
                   <span style="color:#464646;font-weight: normal;font-size:14px">Nos encontramos en una nueva realidad  por eso usted podrá realizar cambios en vuestras fechas de viaje sin costo.</span>
                 </p>
-                <p style="margin:0;font-weight: 600;COLOR:#f9a825;padding:5px 10px;font-size:15px">Pago en Cuotas<br>
-                  <span style="color:#464646;font-weight: normal;font-size:14px">Pague su viaje hasta en 06 cuotas sin intereses, Cuotas de $350,000 Pesos.</span>
-                </p>
+{{--                <p style="margin:0;font-weight: 600;COLOR:#f9a825;padding:5px 10px;font-size:15px">Pago en Cuotas<br>--}}
+{{--                  <span style="color:#464646;font-weight: normal;font-size:14px">Pague su viaje hasta en 06 cuotas sin intereses, Cuotas de $350,000 Pesos.</span>--}}
+{{--                </p>--}}
                 <p style="margin:0;font-weight: 600;COLOR:#f9a825;padding:5px 10px;font-size:15px">Operador Directo<br>
                   <span style="color:#464646;font-weight: normal;font-size:14px">Operador directo de Perú con oficinas en Cusco, Lima y Arequipa. Los mejores Guias locales en cada destino.</span>
                 </p>
@@ -125,48 +125,48 @@
                 </p>
               </td>
             </tr>
-            <tr>
-              <td style="padding:20px 30px 20px 30px;font-size:0;color:#ffffff;background-color:#7F8429;text-align:center">
-                <p style="font-size:17px;font-weight: normal;">Programas de viaje más populares</p>
-                <a href="{{asset('pdf/5_DIAS_CUSCO_EN_COLORES.pdf')}}" target="_blank" style="font-size:15px; color:#f9a825;text-decoration:none"><img src="{{asset('images/pdf.png')}}" width="15" height="15" alt="pdf"> Descargar</a>
-              </td>
-            </tr>
+{{--            <tr>--}}
+{{--              <td style="padding:20px 30px 20px 30px;font-size:0;color:#ffffff;background-color:#7F8429;text-align:center">--}}
+{{--                <p style="font-size:17px;font-weight: normal;">Programas de viaje más populares</p>--}}
+{{--                <a href="{{asset('pdf/5_DIAS_CUSCO_EN_COLORES.pdf')}}" target="_blank" style="font-size:15px; color:#f9a825;text-decoration:none"><img src="{{asset('images/pdf.png')}}" width="15" height="15" alt="pdf"> Descargar</a>--}}
+{{--              </td>--}}
+{{--            </tr>--}}
             <tr>
               <td style="padding:35px 30px 11px 30px;font-size:0;background-color:#404040;border-bottom:1px solid #f0f0f5;border-color:rgba(201,201,207,.35);">
-                <div class="col-sml" style="display:inline-block;width:100%;max-width:270px;vertical-align:top;padding-bottom:10px;font-family:Arial,sans-serif;font-size:13px;line-height:22px;color:#363636;">
-                  <p style="margin:0 0 5px 0;color:#ffffff">
-                    <img src="{{asset('images/phone-call.png')}}" width="15" height="15" alt="celular" style="display:inline-block;color:#cccccc;padding:0 5px">
-                    +51 933 358 040
-                  </p>
-                  <p style="margin:0 0 5px 0;color:#ffffff">
-                    <img src="{{asset('images/email.png')}}" width="15" height="15" alt="email" style="display:inline-block;color:#cccccc;padding:0 5px">
-                    info@gotoperu.com.mx
-                  </p>
-                  <p style="margin:0 0 5px 0;color:#ffffff">
-                    <img src="{{asset('images/maps-and-flags.png')}}" width="15" height="15" alt="ubicacion" style="display:inline-block;color:#cccccc;padding:0 5px">
-                    Av. El Sol 449, Oficina 301.
-                  </p>
-                </div>
+{{--                <div class="col-sml" style="display:inline-block;width:100%;max-width:270px;vertical-align:top;padding-bottom:10px;font-family:Arial,sans-serif;font-size:13px;line-height:22px;color:#363636;">--}}
+{{--                  <p style="margin:0 0 5px 0;color:#ffffff">--}}
+{{--                    <img src="{{asset('images/phone-call.png')}}" width="15" height="15" alt="celular" style="display:inline-block;color:#cccccc;padding:0 5px">--}}
+{{--                    +51 933 358 040--}}
+{{--                  </p>--}}
+{{--                  <p style="margin:0 0 5px 0;color:#ffffff">--}}
+{{--                    <img src="{{asset('images/email.png')}}" width="15" height="15" alt="email" style="display:inline-block;color:#cccccc;padding:0 5px">--}}
+{{--                    info@gotoperu.com.mx--}}
+{{--                  </p>--}}
+{{--                  <p style="margin:0 0 5px 0;color:#ffffff">--}}
+{{--                    <img src="{{asset('images/maps-and-flags.png')}}" width="15" height="15" alt="ubicacion" style="display:inline-block;color:#cccccc;padding:0 5px">--}}
+{{--                    Av. El Sol 449, Oficina 301.--}}
+{{--                  </p>--}}
+{{--                </div>--}}
 
-                <div class="col-lge" style="TEXT-ALIGN:CENTER;display:inline-block;width:100%;max-width:400px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:12px;line-height:22px;color:#363636;">
-                  <p style="margin-top:0;margin-bottom:3px;padding:0;color:#ffffff">SÍGUENOS</p>
-                  <p style="margin:0 0 8px 0;">
-                    <a href="https://www.facebook.com/GOTOPERUcom/" style="text-decoration:none;">
-                      <img src="{{asset('images/logo-de-facebook.png')}}" width="18" height="18" alt="facebook" style="display:inline-block;color:#cccccc;padding:5px">
-                    </a>
-                    <a href="https://www.instagram.com/go.to.peru/" style="text-decoration:none;">
-                      <img src="{{asset('images/instagram.png')}}" width="18" height="18" alt="instagram" style="display:inline-block;color:#cccccc;padding:5px">
-                    </a>
-                    <a href="https://twitter.com/GOTOPERUCOM" style="text-decoration:none;">
-                      <img src="{{asset('images/twitter.png')}}" width="18" height="18" alt="twitter" style="display:inline-block;color:#cccccc;padding:5px">
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCWjJ10j-_BfNTDnmjBug8Ng" style="text-decoration:none;">
-                      <img src="{{asset('images/youtube.png')}}" width="25" height="25" alt="youtube" style="display:inline-block;color:#cccccc;padding:2px 8px">
-                    </a>
-                  </p>
-                </div>
-                <hr style="border-bottom:1px solid #f0f0f5;border-color:rgba(201,201,207,.35);">
-                <p style="margin:0;font-size:11px;line-height:20px;color:#ffffff;text-align:center">&reg; Grupo <span style="color:#f9a825">GOTOPERU</span> 2007 - 2019. Todos los derechos reservados.</p>
+{{--                <div class="col-lge" style="TEXT-ALIGN:CENTER;display:inline-block;width:100%;max-width:400px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:12px;line-height:22px;color:#363636;">--}}
+{{--                  <p style="margin-top:0;margin-bottom:3px;padding:0;color:#ffffff">SÍGUENOS</p>--}}
+{{--                  <p style="margin:0 0 8px 0;">--}}
+{{--                    <a href="https://www.facebook.com/GOTOPERUcom/" style="text-decoration:none;">--}}
+{{--                      <img src="{{asset('images/logo-de-facebook.png')}}" width="18" height="18" alt="facebook" style="display:inline-block;color:#cccccc;padding:5px">--}}
+{{--                    </a>--}}
+{{--                    <a href="https://www.instagram.com/go.to.peru/" style="text-decoration:none;">--}}
+{{--                      <img src="{{asset('images/instagram.png')}}" width="18" height="18" alt="instagram" style="display:inline-block;color:#cccccc;padding:5px">--}}
+{{--                    </a>--}}
+{{--                    <a href="https://twitter.com/GOTOPERUCOM" style="text-decoration:none;">--}}
+{{--                      <img src="{{asset('images/twitter.png')}}" width="18" height="18" alt="twitter" style="display:inline-block;color:#cccccc;padding:5px">--}}
+{{--                    </a>--}}
+{{--                    <a href="https://www.youtube.com/channel/UCWjJ10j-_BfNTDnmjBug8Ng" style="text-decoration:none;">--}}
+{{--                      <img src="{{asset('images/youtube.png')}}" width="25" height="25" alt="youtube" style="display:inline-block;color:#cccccc;padding:2px 8px">--}}
+{{--                    </a>--}}
+{{--                  </p>--}}
+{{--                </div>--}}
+{{--                <hr style="border-bottom:1px solid #f0f0f5;border-color:rgba(201,201,207,.35);">--}}
+                <p style="margin:0;font-size:11px;line-height:20px;color:#ffffff;text-align:center">&reg; Grupo <span style="color:#f9a825">GOTOPERU</span> 2007 - 2026. Todos los derechos reservados.</p>
               </td>
             </tr>
           </table>
