441dc273d04a2a715e1f30ae1f9d152f87bca78b
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2026-03-20 10:20:00 -0500

    fix:

--- resources/views/notifications/page/client-form-design.blade.php
+++ resources/views/notifications/page/client-form-design.blade.php
@@ -95,7 +95,7 @@
 {{--                  <p style="font-size:11px;line-height:14px;color:#6c757d">Reunión Inmediata<br>Horario 09:00a.m 18:00p.m.</p>--}}
 {{--                </div>--}}
 {{--                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">--}}
-{{--                  <a href="https://api.whatsapp.com/send?phone=51922300244" target="_blank"><img src="{{asset('images/whatsapp.png')}}" alt="whatsaap" width="64" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>--}}
+{{--                  <a href="https://api.whatsapp.com/send?phone=51980565849" target="_blank"><img src="{{asset('images/whatsapp.png')}}" alt="whatsaap" width="64" style="margin:0 auto;display:block;border:none;text-decoration:none;color:#363636; padding:0 10px;"></a>--}}
 {{--                </div>--}}
 {{--                <!----}}
 {{--                <div class="" style="display:inline-block;width:100%;max-width:105px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:15px;color:#363636;">--}}
